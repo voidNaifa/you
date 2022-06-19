22baff990d5d87fbe83a585482283920899535a9
Thiago <thiago.naifa@gmail.com>
2022-06-19 02:09:07 -0300

    att

--- you/Assets/Scripts/Worlds/World_01.cs
+++ you/Assets/Scripts/Worlds/World_01.cs
@@ -6,9 +6,9 @@ public class World_01 : MonoBehaviour
 {   
 
     public GameObject bathroom_hall;
-    public GameObject bathroom_bath;
     public GameObject bathroom_wall;
-    public GameObject BLACK_KITCHEN;
+    public GameObject bathroom;
+    public GameObject bedroom;
     public GameObject kitchen;
 
     void Start()
@@ -20,33 +20,35 @@ public class World_01 : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collider)
     {
         if(collider.name == "cl_bedroom_door_out")
-       {
+       {    
+            bathroom.SetActive(true);
+            bedroom.SetActive(false);
             transform.position = new Vector3 (53,135,0);
        } else if(collider.name =="cl_bedroom_door_in") 
        {
+            bathroom.SetActive(false);
+            bedroom.SetActive(true);
             transform.position = new Vector3 (58,3,0);
        }
         if(collider.name =="cl_bathroom_door_in")
        {
             bathroom_hall.SetActive(false);
-            kitchen.SetActive(false);
             bathroom_wall.SetActive(true);
        } else if (collider.name =="cl_bathroom_door_out")
        {
             bathroom_hall.SetActive(true);
-            kitchen.SetActive(true);
             bathroom_wall.SetActive(false);            
        }
         if(collider.name=="cl_kitchen_door_in")
         {
-            bathroom_hall.SetActive(false);
-            bathroom_bath.SetActive(false);
-            BLACK_KITCHEN.SetActive(false);
+            bathroom.SetActive(false);
+            kitchen.SetActive(true);
+            transform.position = new Vector3 (53,504,0);
         } else if(collider.name == "cl_kitchen_door_out")
         {
-            bathroom_hall.SetActive(true);
-            bathroom_bath.SetActive(true);   
-            BLACK_KITCHEN.SetActive(true);         
+            bathroom.SetActive(true);
+            kitchen.SetActive(false);
+            transform.position = new Vector3 (53,331,0);
         }
     }
 
