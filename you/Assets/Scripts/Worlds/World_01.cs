0f9ea96c6679ae711ae253358a4405d44e835957
Thiago <thiago.naifa@gmail.com>
2022-06-19 03:12:22 -0300

    Inicial

--- you/Assets/Scripts/Worlds/World_01.cs
+++ you/Assets/Scripts/Worlds/World_01.cs
@@ -13,7 +13,7 @@ public class World_01 : MonoBehaviour
 
     void Start()
     {
-        transform.position = new Vector3 (-58,-95,0);
+        transform.position = new Vector3 (-60,-78,0);
     }
 
 
@@ -23,12 +23,12 @@ public class World_01 : MonoBehaviour
        {    
             bathroom.SetActive(true);
             bedroom.SetActive(false);
-            transform.position = new Vector3 (53,135,0);
+            transform.position = new Vector3 (53,150,0);
        } else if(collider.name =="cl_bedroom_door_in") 
        {
             bathroom.SetActive(false);
             bedroom.SetActive(true);
-            transform.position = new Vector3 (58,3,0);
+            transform.position = new Vector3 (57,18,0);
        }
         if(collider.name =="cl_bathroom_door_in")
        {
@@ -43,12 +43,12 @@ public class World_01 : MonoBehaviour
         {
             bathroom.SetActive(false);
             kitchen.SetActive(true);
-            transform.position = new Vector3 (53,504,0);
+            transform.position = new Vector3 (53,520,0);
         } else if(collider.name == "cl_kitchen_door_out")
         {
             bathroom.SetActive(true);
             kitchen.SetActive(false);
-            transform.position = new Vector3 (53,331,0);
+            transform.position = new Vector3 (53,344,0);
         }
     }
 
